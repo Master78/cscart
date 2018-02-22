4fa99cb4744b59f165631198786ee7b5790ad101
Master <xultra@yandex.com>
2018-02-22 11:15:30 +0200

    Add files via upload

---
+++ js/addons/master_full_ckeditor/lib/ckeditor/config.js
@@ -0,0 +1,14 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	 config.language = 'ru';
+	 config.uiColor = '#F8B4B7';
+         config.extraPlugins = 'fontawesome';
+         config.contentsCss = '/js/addons/master_full_ckeditor/lib/ckeditor/plugins/fontawesome/font-awesome/4.7.0/css/font-awesome.min.css';
+         config.allowedContent = true;
+
+};
